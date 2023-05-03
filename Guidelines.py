d5eee008f05017379704f2c5d8284e02d273893c
Sean Stephenson <sean.m.stephenson@disney.com>
2023-05-03 11:15:16 -0700

    main: get json from prompt

--- Guidelines.py
+++ Guidelines.py
@@ -30,3 +30,92 @@ load_dotenv()
 
 OPENAI_API_KEY=os.getenv('OPENAI_API_KEY')
 
+
+# In[6]:
+
+
+def get_completion(prompt, model="gpt-3.5-turbo"):
+    messages = [{"role": "user", "content": prompt}]
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=messages,
+        temperature=0,
+    )
+    return response.choices[0].message["content"]
+
+
+# In[7]:
+
+
+f = open("./static-electricity.txt", "r")
+
+
+# In[8]:
+
+
+lines = f.readlines()
+
+
+# In[9]:
+
+
+print(lines)
+
+
+# In[11]:
+
+
+text = lines[0]
+
+
+# In[12]:
+
+
+prompt = f"""Summarize the text delimited by triple backticks into a single sentence. ```{text}```"""
+
+
+# In[14]:
+
+
+openai.api_key = OPENAI_API_KEY
+
+
+# In[15]:
+
+
+response = get_completion(prompt)
+
+
+# In[16]:
+
+
+print(response)
+
+
+# In[17]:
+
+
+prompt = f"""Generate a list of 3 made-up book titles \
+along with their authors and genres. Provide them in \
+JSON format with the following keys: \
+book_id, title, author, genre.
+"""
+
+
+# In[18]:
+
+
+print(prompt)
+
+
+# In[19]:
+
+
+response = get_completion(prompt)
+
+
+# In[20]:
+
+
+print(response)
+
