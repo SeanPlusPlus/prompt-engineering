70f107ffaec9562b0df99cd58265961281b552e2
Sean Stephenson <sean.m.stephenson@disney.com>
2023-05-03 13:44:38 -0700

    main: playground

--- starter.py
+++ starter.py
@@ -1,9 +1,11 @@
 import openai
 import os
 from dotenv import load_dotenv
+from IPython.display import display, HTML
+
 load_dotenv()
-OPENAI_API_KEY=os.getenv('OPENAI_API_KEY')
-openai.api_key = OPENAI_API_KEY
+openai.api_key = os.getenv('OPENAI_API_KEY')
+
 def get_completion(prompt, model="gpt-3.5-turbo"):
     messages = [{"role": "user", "content": prompt}]
     response = openai.ChatCompletion.create(
@@ -13,3 +15,17 @@ def get_completion(prompt, model="gpt-3.5-turbo"):
     )
     return response.choices[0].message["content"]
 
+prompt = f"""
+Your task is to list the ten largest cities in the world by population.
+
+The output should be in an HTML table format.
+
+The table columns should be "City Name", "Country", "Population".
+
+Give the table the title "Largest Citites in the World".
+
+Place the table and the title in a <div> element.
+"""
+
+response = get_completion(prompt)
+display(HTML(response))
