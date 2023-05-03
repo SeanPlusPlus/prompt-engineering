61492f13cba6c8fd626959d1b077708c4b5d0e6f
Sean Stephenson <sean.m.stephenson@disney.com>
2023-05-03 11:01:46 -0700

    main: step back

---
+++ Guidelines.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import openai
+
+
+# In[2]:
+
+
+import os
+
+
+# In[3]:
+
+
+from dotenv import load_dotenv
+
+
+# In[4]:
+
+
+load_dotenv()
+
+
+# In[5]:
+
+
+OPENAI_API_KEY=os.getenv('OPENAI_API_KEY')
+
